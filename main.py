19fc29d843c990b65c740106704d1b83652399a0
mohammadimohammadamin <mohammadimohammadamin697@gmail.com>
2026-06-08 21:28:06 +0330

    Update main.py

--- main.py
+++ main.py
@@ -35,6 +35,25 @@ last_signal = None
 # ====== شروع ======
 send_msg(f"🚀 Bot started\nSymbol={symbol} | TF={tf_input}")
 
+#------------------------------------------------------------------
+
+symbol2 = "SOLUSDT"   # اینجا ارز رو عوض کن
+
+handler2 = TA_Handler(
+    symbol=symbol2,
+    screener="crypto",
+    exchange="BINANCE",
+    interval=interval
+)
+
+# ====== متغیرهای کنترل ======
+prev2_ema20 = None
+prev2_ema50 = None
+last2_signal = None
+
+# ====== شروع ======
+send_msg(f"🚀 Bot started\nSymbol={symbol2} | TF={tf_input}")
+
 # ====== Loop ======
 while True:
     try:
@@ -76,6 +95,49 @@ while True:
         if signal != "NONE" and signal != last_signal:
             send_msg(message)
             last_signal = signal
+
+    except Exception as e:
+        print("Error:", e)
+
+    try:
+        analysis = handler.get_analysis()
+
+        rsi2 = analysis.indicators.get("RSI")
+        ema220 = analysis.indicators.get("EMA20")
+        ema250 = analysis.indicators.get("EMA50")
+
+        print(f"DEBUG -> RSI={rsi2} | EMA20={ema220} | EMA50={ema250}")
+
+        cross2_up = False
+        cross2_down = False
+
+        if prev2_ema20 and prev2_ema50:
+            if prev2_ema20 < prev2_ema50 and ema220 > ema250:
+                cross2_up = True
+            elif prev2_ema20 > prev2_ema50 and ema220 < ema250:
+                cross2_down = True
+
+        prev2_ema20 = ema220
+        prev2_ema50 = ema250
+
+        # ====== شرط‌ها ======
+        if rsi2 >= 58 and cross2_up:
+            signal2 = "BUY"
+        elif rsi2 <= 42 and cross2_down:
+            signal2 = "SELL"
+        else:
+            signal2 = "NONE"
+
+        # ====== پیام ======
+        if signal2 == "BUY":
+            message = f"{symbol2} | {tf_input}\n📈 BUY SIGNAL\nRSI={rsi2:.2f}"
+        elif signal == "SELL":
+            message = f"{symbol2} | {tf_input}\n📉 SELL SIGNAL\nRSI={rsi2:.2f}"
+
+        # ====== ارسال فقط در صورت سیگنال جدید ======
+        if signal2 != "NONE" and signal2 != last_signal2:
+            send_msg(message)
+            last_signal2 = signal2
 
     except Exception as e:
         print("Error:", e)
